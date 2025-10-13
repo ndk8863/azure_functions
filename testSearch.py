d6e61b8b363cdcbbf31f88ad87f12508aa718cc1
ndk8863 <nishiokadaiki9@gmail.com>
2025-10-13 10:24:12 +0900

    保存機能追加

--- testSearch.py
+++ testSearch.py
@@ -3,6 +3,9 @@
 import requests
 import xml.etree.ElementTree as ET
 import json
+import pandas as pd
+import os
+
 url = "https://ndlsearch.ndl.go.jp/api/opensearch"
 params = {
     "cnt": 5,
@@ -11,20 +14,13 @@ params = {
 
 res = requests.get(url,params = params)
 xml_data = res.text
-# res = requests.get(url)
-# data = res.json()
-# print(data)
-print(xml_data)
-
 root = ET.fromstring(xml_data)
-
 items = []
-
 for item in root.findall(".//item"):
     data = {
         "title": item.findtext("title"),
         "link": item.findtext("link"),
-        "creator": item.findtext("{*}creator"),  # 名前空間があるので{*}でマッチ
+        "creator": item.findtext("{*}creator"),  
         "series": item.findtext("{*}seriesTitle"),
         "publisher": item.findtext("{*}publisher"),
         "date": item.findtext("{*}date"),
@@ -32,8 +28,12 @@ for item in root.findall(".//item"):
     }
     items.append(data)
 
-json_output = json.dumps(items,ensure_ascii=False,indent=2)
-print(json_output)
+df = pd.DataFrame(items)
 
-# for item in data["response"]["items"]:
-#     print(item["title"],"-",item.get("creator","不明"))
+current_dir_path = os.path.dirname(os.path.abspath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(current_dir_path,os.pardir))
+
+save_file_path = os.path.join(parent_dir_path,"save/tmp.csv")
+
+df.to_csv(save_file_path,encoding='utf8')
+
